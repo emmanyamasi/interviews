c939f60e79687ed4dbce283fbeb5f060b2f8c47f
Emma Askari <2004emmanyamasi@gmail.com>
2026-01-06 14:34:16 +0100

    binarysearch

---
+++ Binarysearch/example1.py
@@ -0,0 +1,31 @@
+#works in a sorted array
+#instead of checking every element check the middle and throw away half the array each time
+
+#O(log n)time
+
+
+#you are given a sorted arrray of integers nums  and an integet target
+#if the target does not exist return -1
+
+nums = [1, 3, 5, 7, 9, 11]
+target = 7
+
+
+def binary_search(nums,target):
+    left =0
+    right = len(nums)-1
+
+    while left<=right:
+        mid = (left+right)//2
+        if nums[mid] ==target:
+            return mid
+        elif nums[mid] <target:
+            left = mid+1
+
+        else:
+            right = mid -1
+
+    return -1
+
+
+print("output",binary_search(nums,target))
---
+++ Binarysearch/example3.py
@@ -0,0 +1,50 @@
+#You are given a sorted array of integers nums and an integer target.
+
+#Return the index of the FIRST occurrence of target.
+#If the target does not exist, return -1.
+
+nums = [1, 2, 2, 2, 3, 4, 5]
+target = 2
+
+
+def occur(nums,target):
+    left =0
+    right = len(nums)-1
+    result =-1  #stores the first occurrence if found
+    while left<right:
+        mid = (left+right)//2
+        if nums[mid] == target:
+            result= mid #at first you find target you stopbut now we have to keep going left to find fist occurence
+            right = mid -1 #go left and find an earlier one
+        elif nums[mid] < target:
+            left +=1
+        else :
+            right+=1
+
+    return result
+    
+
+print("output",occur(nums,target))
+
+
+
+def finaloccur(nums,target):
+    left =0
+    right =len(nums)-1
+    result =-1
+
+    while left<=right:
+        mid = (left +right)//2
+
+        if nums[mid] == target:
+            result =mid
+            left = mid +1 #move to the right and find the closest one
+        elif nums[mid]<target:
+            left =mid+1
+        else:
+            right = mid -1
+
+    return result
+
+print("output",finaloccur(nums,target))
+
